18d27e869a0204b4650c571543b80090af273640
GG337 <58821279+GG337@users.noreply.github.com>
2023-07-22 10:38:22 -0500

    Remove main function

--- BANK_MANAGEMENT_SYSTEM.py
+++ BANK_MANAGEMENT_SYSTEM.py
@@ -80,26 +80,3 @@ def CloseAcc():
     mydb.commit()
     print("Data Entered Successfully")
     main()
-
-
-
-
-def main():
-    print('''1. Open New Account\n2. Deposit Amount\n3. Withdraw Amount\n4. Balance Enquiry\n5. Dispaly Customer Details\n6. Close An Account''')
-    choice =input('Enter The Option You Want: ')
-    if (choice =='1'):
-        OpenAcc()
-    elif(choice =='2'):
-        DepoAmo()
-    elif(choice =='3'):
-        WithdrawAmount()
-    elif(choice =='4'):
-        BalEnq()
-    elif(choice =='5'):
-        DisDetails()
-    elif(choice =='6'):
-        CloseAcc()
-    else:
-        print('Invalid Choice')
-        main()
-main()
