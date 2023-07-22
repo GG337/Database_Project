dde2849bab1b096ab27db99ed1577a02b2694eec
GG337 <58821279+GG337@users.noreply.github.com>
2023-07-22 10:33:59 -0500

    Add main function to main file

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
-import sys
+from BANK_MANAGEMENT_SYSTEM import
 
-def say_hi(name: str):
-    print(f'Hi! {name}')
-
-
-def add_these(conf: int,seed: int):
-    print(conf,seed)
-
-if __name__ == '__main__':
-    args = sys.argv[1:]
-    if args:
-        function_name = args[0]
-        if function_name == say_hi.__name__:
-            say_hi()
-        elif function_name == add_these.__name__:
-            if not len(args) == 3:
-                print('You either provided too many or not enough arguements to use this function. ')
-            else:
-                try:
-                    conf, seed = int(args[1]), int(args[2])
-                    add_these(conf,seed)
-                    if conf == 1:
-                        print(f'Conference is West Coast and is the {seed} seed. :)')
-                    elif conf == 2:   
-                        print(f'Conference is East Coast and is the {seed} seed. :)')
-
-                except ValueError:
-                    print("One of the arguements provided was a number.")
-
+def main():
+    print('''1. Open New Account\n2. Deposit Amount\n3. Withdraw Amount\n4. Balance Enquiry\n5. Dispaly Customer Details\n6. Close An Account''')
+    choice =input('Enter The Option You Want: ')
+    if (choice =='1'):
+        OpenAcc()
+    elif(choice =='2'):
+        DepoAmo()
+    elif(choice =='3'):
+        WithdrawAmount()
+    elif(choice =='4'):
+        BalEnq()
+    elif(choice =='5'):
+        DisDetails()
+    elif(choice =='6'):
+        CloseAcc()
     else:
-        print('No arguement was provided')
+        print('Invalid Choice')
+        main()
+main()
